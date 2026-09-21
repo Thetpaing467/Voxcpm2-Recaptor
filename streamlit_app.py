b58e9ed0ec5faa63b960be3ac1691e7e192f01d5
Thetpaing467 <3142thetpaing@gmail.com>
2026-09-21 18:35:32 +0630

    Refactor call_gemini function position and indentation
    Fix indentation and move call_gemini function.

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,8 @@ class KeyManager:
         return False
 
     def remaining(self):
-        return len(self.keys) - len(self.exhausted)
-      def call_gemini(contents, km):
+        return len(self.keys) - len(self.exhausted)   
+def call_gemini(contents, km):
     attempts = 0
     max_total = len(km.keys) * 3 if km.keys else 3
     while attempts < max_total:
